9e587cd86fc144dbb2bc78cf211c3b96119b0ff7
Tully Foote <tfoote@osrfoundation.org>
2021-04-07 14:20:18 -0700

    analyze awstats for 2020

--- scripts/analyze_awstats.py
+++ scripts/analyze_awstats.py
@@ -3,14 +3,15 @@
 import argparse
 import os
 
-UBUNTU_DISTROS = ['precise', 'quantal', 'raring', 'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan']
-DEBIAN_DISTROS = ['jessie', 'stretch', 'buster']
+UBUNTU_DISTROS = ['precise', 'quantal', 'raring', 'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan', 'focal']
+DEBIAN_DISTROS = ['jessie', 'stretch', 'buster', 'bullseye']
 OS_DISTROS = UBUNTU_DISTROS + DEBIAN_DISTROS
 ARCHES = ['i386', 'amd64', 'armhf', 'arm64', 'source']
 ROS1_DISTROS = ['boxturtle', 'cturtle', 'diamondback', 'electric', 'fuerte', 'groovy', 'hydro', 'indigo', 'jade', 'kinetic', 'lunar', 'melodic', 'noetic']
-ROS2_DISTROS = ['ardent', 'bouncy', 'crystal', 'dashing', None]
+ROS2_DISTROS = ['ardent', 'bouncy', 'crystal', 'dashing', 'eloquent', 'foxy', 'galactic']
+ROS2_ROLLING_DISTROS = ['rolling']
 
-ROS_DISTROS = ROS1_DISTROS + ROS2_DISTROS
+ROS_DISTROS = ROS1_DISTROS + ROS2_DISTROS + ROS2_ROLLING_DISTROS
 
 MIN_DATA_THRESHOLD = 0.001
 
