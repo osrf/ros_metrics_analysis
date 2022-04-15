d75866f197fc2124b85a00a5a81178098804d078
Brian Gerkey <gerkey@osrfoundation.org>
2022-04-15 00:41:53 -0700

    tweak year range

--- scripts/get-us-hits.bash
+++ scripts/get-us-hits.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-for y in 2016 2017 2018 2019 2020 2021; do
+for y in 2017 2018 2019 2020 2021; do
   echo -n "$y	"
   grep "^us " awstats??$y.packages.ros.org.txt | awk {'print $3'} | awk '{s+=$1} END {print s}'
 done
