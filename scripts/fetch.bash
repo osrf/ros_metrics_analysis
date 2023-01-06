7aff5ad14222567eba4a22eaa9cccafe6ce05c6d
Tully Foote <tfoote@osrfoundation.org>
2023-01-06 14:06:07 -0800

    pull edit-log too

--- scripts/fetch.bash
+++ scripts/fetch.bash
@@ -1,1 +1,2 @@
-rsync -ave ssh osuoslros:~/awstats/* . --bwlimit 500 --progress --partial
+rsync -ave ssh ros@ros1.osuosl.org:~/awstats/* . --bwlimit 5000 --progress --partial
+rsync -ave ssh ros@ros1.osuosl.org:/var/www/wiki.ros.org/data/edit-log . --bwlimit 5000 --progress --partial
